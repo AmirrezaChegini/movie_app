7e58512927080e91f72fd9f44ef315a76fbc7846
AmirrezaChegini <amirreza.chegini.1380@gmail.com>
2023-09-10 22:11:51 +0330

    feat: add login and registerapi

---
+++ lib/core/constants/api.dart
@@ -0,0 +1,4 @@
+class Api {
+  static const String register = 'api/v1/register';
+  static const String login = 'oauth/token';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movie_app/config/theme.dart';
 import 'package:movie_app/core/utils/app_routes.dart';
+import 'package:movie_app/di.dart';
 import 'package:movie_app/features/auth/ui/cubit/pass_visible_cubit.dart';
 
-void main() {
+void main() async {
+  await initLocator();
   runApp(const MainApp());
 }
 
